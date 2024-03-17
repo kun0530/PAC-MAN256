5b68feaec5bd91e668a5613b9bad8fb21cfe3deb
박형곤 <h.gon0530@gmail.com>
2024-03-17 14:49:58 +0900

    킬 스크린 애니메이션 구현

--- PacMan256/GameObjects/KillScreen.cpp
+++ PacMan256/GameObjects/KillScreen.cpp
@@ -2,26 +2,50 @@
 #include "KillScreen.h"
 #include "SceneGame.h"
 #include "TileMap.h"
+#include "SpriteGo.h"
 
-KillScreen::KillScreen(const std::string& name) : SpriteGo(name)
+KillScreen::KillScreen(const std::string& name) : GameObject(name)
 {
 }
 
 void KillScreen::Init()
 {
-	SpriteGo::Init();
+	GameObject::Init();
 
-	SetTexture("graphics/glitch-map-1.png");
+	blackScreen.setFillColor(sf::Color::Black);
+	blackScreen.setSize((sf::Vector2f)FRAMEWORK.GetWindowSize() * 0.78125f);
+	Utils::SetOrigin(blackScreen, Origins::TL);
+
+	for (int i = 0; i <= 3; ++i)
+	{
+		for (int j = 0; j < 50; ++j)
+		{
+			SpriteGo* glitchMap = new SpriteGo("Glitch Map");
+			glitchMap->SetTexture("graphics/glitch-map-" + std::to_string(i) + ".png");
+			glitchMap->SetOrigin(Origins::TL);
+			killScreenParts.push_back(glitchMap);
+		}
+	}
 }
 
 void KillScreen::Release()
 {
-	SpriteGo::Release();
+	GameObject::Release();
+
+	for (auto parts : killScreenParts)
+	{
+		if (parts != nullptr)
+		{
+			delete parts;
+			parts = nullptr;
+		}
+	}
+	killScreenParts.clear();
 }
 
 void KillScreen::Reset()
 {
-	SpriteGo::Reset();
+	GameObject::Reset();
 
 	sceneGame = dynamic_cast<SceneGame*>(SCENE_MGR.GetCurrentScene());
 	tileMapId = sceneGame->GetCurrentTileMapId();
@@ -32,14 +56,21 @@ void KillScreen::Reset()
 		tileMap = sceneGame->GetPrevTileMap();
 		--tileMapId;
 	}
-	SetOrigin(Origins::ML);
+	SetOrigin(Origins::TL);
 	
 	gridIndexY = tileMap->GetCellCount().y - 1;
 	nextGridindexY = gridIndexY - 1;
 
-	currentPosition = tileMap->GetGridPosition(0, gridIndexY);
-	nextPosition = tileMap->GetGridPosition(0, nextGridindexY);
+	cellBLPos = { -tileMap->GetCellSize().x / 2.f, tileMap->GetCellSize().y / 2.f };
+	currentPosition = tileMap->GetGridPosition(0, gridIndexY) + cellBLPos;
+	nextPosition = tileMap->GetGridPosition(0, nextGridindexY) + cellBLPos;
 	SetPosition(currentPosition);
+
+	partsUpdateTimer = partsUpdateDuration;
+	for (auto parts : killScreenParts)
+	{
+		parts->SetPosition(sf::Vector2f(0.f, FRAMEWORK.GetWindowSize().y));
+	}
 
 	moveTime = Utils::Magnitude(nextPosition - currentPosition) / speed;
 	timer = 0.f;
@@ -47,7 +78,25 @@ void KillScreen::Reset()
 
 void KillScreen::Update(float dt)
 {
-	SpriteGo::Update(dt);
+	GameObject::Update(dt);
+
+	blackScreen.setPosition(position);
+
+	for (auto parts : killScreenParts)
+	{
+		if (partsUpdateTimer >= partsUpdateDuration)
+		{
+			float randPosX = Utils::RandomRange(-100.f, blackScreen.getSize().x);
+			float randPosY = Utils::RandomRange(-30.f, 100.f);
+			parts->SetPosition(position + sf::Vector2f(randPosX, randPosY));
+			partsUpdateTimer = 0.f;
+		}
+		else
+		{
+			partsUpdateTimer += dt;
+			parts->Translate(direction * speed * dt);
+		}
+	}
 
 	if (sceneGame->GetCurrentTileMapId() - tileMapId >= 2)
 		Reset();
@@ -66,7 +115,7 @@ void KillScreen::Update(float dt)
 			gridIndexY = tileMap->GetCellCount().y;
 			nextGridindexY = gridIndexY - 1;
 		}
-		nextPosition = tileMap->GetGridPosition(0, nextGridindexY);
+		nextPosition = tileMap->GetGridPosition(0, nextGridindexY) + cellBLPos;
 
 		moveTime = Utils::Magnitude(nextPosition - currentPosition) / speed;
 		timer = 0.f;
@@ -81,10 +130,17 @@ void KillScreen::Update(float dt)
 
 void KillScreen::FixedUpdate(float dt)
 {
-	SpriteGo::FixedUpdate(dt);
+	GameObject::FixedUpdate(dt);
 }
 
 void KillScreen::Draw(sf::RenderWindow& window)
 {
-	SpriteGo::Draw(window);
+	GameObject::Draw(window);
+
+	window.draw(blackScreen);
+
+	for (auto parts : killScreenParts)
+	{
+		parts->Draw(window);
+	}
 }
--- PacMan256/GameObjects/KillScreen.h
+++ PacMan256/GameObjects/KillScreen.h
@@ -1,14 +1,18 @@
 #pragma once
-#include "SpriteGo.h"
+#include "GameObject.h"
 
 class SceneGame;
 class TileMap;
+class SpriteGo;
 
-class KillScreen : public SpriteGo
+class KillScreen : public GameObject
 {
 private:
 	SceneGame* sceneGame = nullptr;
 	TileMap* tileMap = nullptr;
+
+	sf::RectangleShape blackScreen;
+	std::vector<SpriteGo*> killScreenParts;
 
 	int tileMapId = 0;
 	int gridIndexY = 0;
@@ -16,12 +20,16 @@ private:
 
 	sf::Vector2f currentPosition = { 0.f, 0.f };
 	sf::Vector2f nextPosition = { 0.f, 0.f };
+	sf::Vector2f cellBLPos = { 0.f, 0.f };
 
 	float speed = 100.f;
 	const sf::Vector2f direction = { 0.f, -1.f };
 
 	float timer = 0.f;
 	float moveTime = 0.f;
+
+	float partsUpdateTimer = 0.f;
+	float partsUpdateDuration = 0.1f;
 
 public:
 	KillScreen(const std::string& name = "");
